af84197c7b191fde202a737b9a3b6c15d328210b
arojaspe <arojaspe@unal.edu.co>
2025-01-10 20:35:24 -0500

    feat: 💄 add LoginCard component | create and route links and pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import HomeCard from "./components/homeCard";
 export default function App() {
   return (
     <>
-      <div className="flex justify-between items-center bg-white min-h-screen overflow-hidden">
+      <div className="flex justify-between items-center bg-backgroundClassMatch min-h-screen overflow-hidden">
         <div className="w-full flex  justify-center ">
           <HomeCard containerClassName="w-[80%]" />
         </div>
