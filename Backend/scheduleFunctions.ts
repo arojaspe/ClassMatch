14cfefb28a014c1ec5b945bdcd52fd51c01ae028
dafty-punky-boy <criosro@unal.edu.co>
2025-02-01 14:44:03 -0500

    schedule iterfaces first version

--- Backend/scheduleFunctions.ts
+++ Backend/scheduleFunctions.ts
@@ -1,7 +1,25 @@
 import * as Models from "./Models";
 
-interface CodedSchedule;
-interface DecodedSchedule;
+interface CodedSchedule{
+    Monday: number;
+    Tueday: number;
+    Wednesday: number;
+    Thursday: number;
+    Friday: number;
+    Saturday: number;
+    Sunday: number;
+}
+
+// [0-1, 1-2, ..., 22-23, 23-24]
+interface DecodedSchedule{
+    Monday: boolean[];
+    Tueday: boolean[];
+    Wednesday: boolean[];
+    Thursday: boolean[];
+    Friday: boolean[];
+    Saturday: boolean[];
+    Sunday: boolean[];
+}
 
 function decodeSchedule(){}
 function codeSchedule(){}
