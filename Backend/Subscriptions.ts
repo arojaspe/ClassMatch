efb6eee45635ec4db7e7d844d2919eb85d64eedf
dafty-punky-boy <criosro@unal.edu.co>
2025-02-08 21:32:57 -0500

    First mercado pago implementation

--- Backend/Subscriptions.ts
+++ Backend/Subscriptions.ts
@@ -3,28 +3,41 @@ import * as Funcs from "./Functions";
 import {MercadoPagoConfig, PreApproval} from "mercadopago";
 import * as Models from "./Models";
 import dotenv from "dotenv";
+import * as crypto from "crypto";
+
 
 dotenv.config();
 const accessToken = process.env.MERCADO_PAGO_ACCESS_TOKEN as string;
+const secret = process.env.WEB_HOOK_SECRET as string;
 export const client = new MercadoPagoConfig({accessToken: accessToken});
 
+
+// A premium user will have both the premium plan and the api_id
+// A premium user with paused pays will have no premium plan but api_id
+// A basic user will have null api_id and basic plan
+
 export const createSubscription = async (req: Request, res: Response) => {
+    const userEmail = req.body.email;
     try {
         const currUser = await Funcs.isLoggedIn(req, res);
         const subscription = await new PreApproval(client).create({
             body: {
-                back_url: "http://localhost:5000/api/",
+                back_url: "https://aca1-200-118-80-60.ngrok-free.app/api/",
                     reason: "ClassMatch Premium",
                 auto_recurring: {
                     frequency: 1,
                     frequency_type: "months",
-                    transaction_amount: 13900,
+                    transaction_amount: 15900,
                     currency_id: "COP",
                 },
+                payer_email: userEmail,
                 status: "pending",
                 //notification_url: "https://aca1-200-118-80-60.ngrok-free.app/api/webhook",
             },
         });
+
+        console.log(subscription);
+        console.log(currUser.USER_ID);
 
         const [updatedRows] = await Models.SUBSCRIPTIONS_MOD.update(
             {SUBSCRIPTION_API_ID : subscription.id, SUBSCRIPTION_PLAN: "Basic"},
